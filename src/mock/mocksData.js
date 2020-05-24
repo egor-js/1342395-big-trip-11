7862a3c970a55c24a8c2478a958cc15273790b7e
Egor <egor@serverice.ru>
2020-05-24 18:45:50 +0700

    WIP

--- src/mock/mocksData.js
+++ src/mock/mocksData.js
@@ -1,4 +1,6 @@
-const typeTripPoint = [`Taxi`, `Bus`, `Train`, `Ship`, `Transport`, `Drive`, `Flight`, `Check-in`, `Sightseeing`, `Restaurant`];
+import {utils} from "../utils.js";
+
+const typeTripPoint = [`taxi`, `bus`, `train`, `ship`, `transport`, `drive`, `flight`, `check-in`, `sightseeing`, `restaurant`];
 const tripCitys = [`Amsterdam `, `Chamonix`, `Geneva`];
 
 const descriptionTripPoint = [`Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
@@ -13,4 +15,145 @@ const descriptionTripPoint = [`Lorem ipsum dolor sit amet, consectetur adipiscin
   `Nunc fermentum tortor ac porta dapibus.`,
   `In rutrum ac purus sit amet tempus.`];
 
-export {typeTripPoint, tripCitys, descriptionTripPoint};
+
+const offersByPointType = {
+  "drive": [
+    {
+      "title": `Rent a car`,
+      "price": 200
+    },
+    {
+      "title": `Eat in car`,
+      "price": 30
+    },
+  ],
+  "bus": [
+    {
+      "title": `Choose meal`,
+      "price": 180
+    },
+    {
+      "title": `Upgrade to comfort class`,
+      "price": 50
+    },
+  ],
+  "flight": [
+    {
+      "title": `Add luggage`,
+      "price": 30
+    },
+    {
+      "title": `Switch to comfort class`,
+      "price": 100
+    },
+    {
+      "title": `Add meal`,
+      "price": 15
+    },
+    {
+      "title": `Choose seats`,
+      "price": 5
+    },
+    {
+      "title": `Travel by train`,
+      "price": 40
+    },
+  ],
+  "taxi": [
+    {
+      "title": `Order Uber`,
+      "price": 20
+    }
+  ],
+  "check-in": [
+    {
+      "title": `Add breakfast`,
+      "price": 50
+    }
+  ],
+  "sightseeing": [
+    {
+      "title": `Book tickets`,
+      "price": 40
+    },
+    {
+      "title": `Lunch in city`,
+      "price": 30
+    }
+  ],
+};
+
+const destinanion = [
+  {
+    "description": `Chamonix, is a beautiful city, a true asian pearl, with crowded streets.`,
+    "name": `Chamonix`,
+    "pictures": [
+      {
+        "src": `http://picsum.photos/248/152?r=${Math.random()}`,
+        "description": `Chamonix parliament building`
+      }
+    ]
+  },
+  {
+    "description": `Amsterdam is a capital of the Smoking World`,
+    "name": `Amsterdam`,
+    "pictures": [
+      {
+        "src": `http://picsum.photos/248/152?r=${Math.random()}`,
+        "description": `Red lighting street in Amsterdam`
+      }
+    ]
+  },
+  {
+    "description": `Geneva is the second-most populous city in Switzerland (after Zürich) and the most populous city of Romandy, the French-speaking part of Switzerland.`,
+    "name": `Geneva`,
+    "pictures": [
+      {
+        "src": `http://picsum.photos/248/152?r=${Math.random()}`,
+        "description": `The Jet d'Eau is a large fountain in Geneva,`
+      }
+    ]
+  },
+
+
+];
+
+// const point = {
+//   "base_price": 1100,
+//   "date_from": `2019-07-10T22:55:56.845Z`,
+//   "date_to": `2019-07-11T11:22:13.375Z`,
+//   "destination": destinanion,
+//   "id": `0`,
+//   "is_favorite": false,
+//   "offers": [
+//     {
+//       "title": `Choose meal`,
+//       "price": 180
+//     }, {
+//       "title": `Upgrade to comfort class`,
+//       "price": 50
+//     }
+// ],
+// "type": `bus`;
+
+const tripPointsMocks = [];
+const tempRandomOffers = [];
+
+const createTripPoints = function () {
+  for (let i = 0; i < 10; i++) {
+    // const basePriceRandom = ;
+    const randomType = typeTripPoint[utils.randomInt(9)];
+    // tempRandomOffers[i] = randomType;// offersByPointType.bus;
+    tripPointsMocks.push({
+      "base_price": utils.randomInt(500, 2000),
+      "destinanion": destinanion[utils.randomInt(2)],
+      "id": i,
+      "is_favorite": Math.random() > 0.5,
+      "type": randomType,
+      "offers": offersByPointType[randomType],
+    });
+
+  }
+};
+// typeTripPoint, tripCitys, descriptionTripPoint, offersByPointType,
+export { createTripPoints, tripPointsMocks, tempRandomOffers};
